0f688198db8b50468ed4bab9433ce16d6321971c
Annabelle Fait <imannaf8@gmail.com>
2025-02-09 17:36:43 -0500

    Task 1 - Fixed Comments

--- cc_6.js
+++ cc_6.js
@@ -2,6 +2,6 @@
 
 function calculateProfit(costPrice, sellingPrice, unitsSold){
     return (sellingPrice - costPrice) * unitsSold;
-};
-console.log("Total Profit: $", calculateProfit(20, 30, 100))
-console.log("Total Profit: $", calculateProfit(50, 70, 200))
+};// function to calculate the profit of a product
+console.log("Total Profit: $", calculateProfit(20, 30, 100)) // logging the profit from a product that costs $20, sells for $30 and sold 100 units
+console.log("Total Profit: $", calculateProfit(50, 70, 200)) // logging the profit from a product that costs $50, sells for $70 and sold 200 units
