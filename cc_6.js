4c55f719d6d79d339c1009c29871298b7e604b7f
Annabelle Fait <imannaf8@gmail.com>
2025-02-09 22:13:15 -0500

    Task - 4 Subscription Pricing Model

--- cc_6.js
+++ cc_6.js
@@ -9,7 +9,23 @@ console.log("Total Profit: $", calculateProfit(50, 70, 200)) // logging the prof
 // Task 2 - Sales Tax Computation
 
 function calculateSalesTax(amount, taxRate){
-    return(amount * taxRate)
+    return (amount * taxRate)
 }; // crreate function that calculates sales tax based on the amount and taxrate
 console.log("Sales Tax: $", Math.floor(calculateSalesTax(100,0.07))); // calculates sales tax for $100 using a tax rate of 0.07
 console.log("Sales Tax: $", Math.floor(calculateSalesTax(500,0.1))); // calculates sales tax for $500 using a tax rate of 0.1
+
+// Task 3 - Employee Bonus Calculation
+
+const calculateBonus = (salary, performanceRating) => { // create function that calculates bonuses based on salary and performance rating
+    if (performanceRating === "Excellent") {
+        return (salary * .20);
+    }; // returns a 20% bonus for an excellent performance rating
+    if (performanceRating === "Good") {
+        return (salary * .10);
+    }; // returns a 10% bonus for a good performance rating
+    if (performanceRating === "Average") {
+        return (salary * .05);
+    }; // returns a 5% bonus for an average performance rating
+};
+console.log("Bonus: $", calculateBonus(5000, "Excellent")); // logging the bonus for an excellent performance rating
+console.log("Bonus: $", calculateBonus(7000, "Good")); // loggin the bonus for a good performance rating
